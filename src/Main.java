c4c86143cfdae7f690b74ce01c1fc921a508f9b3
Adeeba Khan <i190626@nu.edu.pk>
2021-09-12 11:02:51 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World!!!\n"); // print only prints
+		System.out.println("Java here"); // while print line prints a line too
+
+	}
+}
